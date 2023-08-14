faa5b0644afc935e668f6b4d823e8d935c9ba698
tmojulat <140878712+tmojulat@users.noreply.github.com>
2023-08-14 18:51:53 +0800

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -43,8 +43,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", `response.data.weather[0].description`);
 }
 
-let apiKey = "1fa00e65cf105b1c9ae0d3141eca3cfd";
-let city = "Kota Kinabalu";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "1fa00e65cf105b1c9ae0d3141eca3cfd";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Kota Kinabalu");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
