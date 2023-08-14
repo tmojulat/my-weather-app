055ecb4ea10a6766955d98296093454ca259e039
tmojulat <140878712+tmojulat@users.noreply.github.com>
2023-08-14 16:27:56 +0800

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "1fa00e65cf105b1c9ae0d3141eca3cfd";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kota Kinabalu&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
